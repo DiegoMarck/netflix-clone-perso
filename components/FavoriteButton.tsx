c82b1cd222d6605a1f3f519e56c6b4503aafcea7
Diego de Massy <ddemassy@gmail.com>
2023-10-22 09:23:53 +0200

    résolution bug identification github + index.tsx
    résolution + index.tsx

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useCallback, useMemo } from 'react';
-// import { PlusIcon, CheckIcon } from '@heroicons/react/24/outline';
-import { AiOutlinePlus } from 'react-icons/ai';
+import { AiOutlinePlus, AiOutlineCheck } from 'react-icons/ai';
 
 import useCurrentUser from '@/hooks/useCurrentUser';
 import useFavorites from '@/hooks/useFavorites';
@@ -11,40 +10,41 @@ interface FavoriteButtonProps {
 }
 
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
-  // const { mutate: mutateFavorites } = useFavorites();
+  const { mutate: mutateFavorites } = useFavorites();
 
-  // const { data: currentUser, mutate } = useCurrentUser();
+  const { data: currentUser, mutate } = useCurrentUser();
 
-  // const isFavorite = useMemo(() => {
-  //   const list = currentUser?.favoriteIds || [];
+  const isFavorite = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
 
-  //   return list.includes(movieId);
-  // }, [currentUser, movieId]);
+    return list.includes(movieId);
+  }, [currentUser, movieId]);
 
-  // const toggleFavorites = useCallback(async () => {
-  //   let response;
+  const toggleFavorites = useCallback(async () => {
+    
+    let response;
 
-  //   if (isFavorite) {
-  //     response = await axios.delete('/api/favorite', { data: { movieId } });
-  //   } else {
-  //     response = await axios.post('/api/favorite', { movieId });
-  //   }
+    if (isFavorite) {
+      response = await axios.delete('/api/favorite', { data: { movieId } });
+    } else {
+      response = await axios.post('/api/favorite', { movieId });
+    }
 
-  //   const updatedFavoriteIds = response?.data?.favoriteIds;
+    const updatedFavoriteIds = response?.data?.favoriteIds;
 
-  //   mutate({ 
-  //     ...currentUser, 
-  //     favoriteIds: updatedFavoriteIds,
-  //   });
-  //   mutateFavorites();
-  // }, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
+    mutate({ 
+      ...currentUser, 
+      favoriteIds: updatedFavoriteIds,
+    });
+    mutateFavorites();
+    
+  }, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
   
-  // const Icon = isFavorite ? CheckIcon : PlusIcon;
+  const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
 
   return (
-    <div className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
-      {/* <Icon className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" /> */}
-      <AiOutlinePlus className='text-white size={25}'/>
+    <div onClick={toggleFavorites} className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+     <Icon className='text-white size={25}'/>
     </div>
   )
 }
